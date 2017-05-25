563e1a82c23aa867c428d222798e9b676b6bfd2f
Samuel Tauil <samueltauil@gmail.com>
2017-05-25 16:12:26 -0400

    magic, have no clue but it works

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ git add .
 git commit -am "$(curl -s http://whatthecommit.com/index.txt 2>&1)"
 git push
 oc delete all --all -n labs
-oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master/_module_groups.yml"
+oc new-app samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshift-app-framework/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshift-app-framework/master/_module_groups.yml"
